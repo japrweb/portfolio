7d950a0052d6865781bf47e62c3933906ae6fa85
John A. Pérez R. <74509783+japrweb@users.noreply.github.com>
2025-01-04 15:15:13 +0000

    Transparent_Bg_Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,34 +4,47 @@ import { Link } from "react-scroll";
 
 function Home() {
   return (
-    <div name="home" className="w-full h-screen bg-[#0e0e0e] md:px-10">
-      {/* Container */}
-      
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        
-        <p className="text-[#6BD425]">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          John A. Pérez R.
-        </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Front End Developer
-        </h2>
-        <p className="text-gray-300 py-4 max-w-[630px]">
-          I'm skilled in web creation and design, including layout using tools such as Figma and Miro. Able to provide guidance and employ agile methodologies like Scrum and Kanban. Proficient in creating wireframes, site maps, and flowcharts to ensure optimal user experience
-        </p>
-        <div>
-          <Link to="work" smooth={true} duration={500} delay={300} className="flex items-center">
-            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#6BD425] hover:border-[#6BD425] hover:text-[#0e0e0e] font-medium rounded-md duration-700 z-10">
-              View Work
-              <span className="group-hover:rotate-90 duration-300">
-                <HiArrowNarrowRight className="ml-3" />
-              </span>
-            </button>
-          </Link>
-        </div>
-        <div className="shadow-inset-white bg-cover animate-myAnimation bg-center bg-no-repeat w-[85%] h-[50%] md:w-[70%] lg:w-[40%] md:h-[60%] absolute z-0 -mt-20 opacity-50"/>
-      </div>
+    <div
+  name="home"
+  className="relative bg-[url('https://images.unsplash.com/photo-1484417894907-623942c8ee29?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center bg-no-repeat h-screen w-full"
+>
+  {/* Glassmorphism Layer */}
+  <div className="absolute inset-0 bg-black bg-opacity-70 backdrop-blur-sm"></div>
+
+  {/* Container */}
+  <div className="relative max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+    <p className="text-[#6BD425]">Hi, my name is</p>
+    <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+      John A. Pérez R.
+    </h1>
+    <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+      I'm a Front End Developer
+    </h2>
+    <p className="text-gray-300 py-4 max-w-[630px]">
+      I'm skilled in web creation and design, including layout using tools such
+      as Figma and Miro. Able to provide guidance and employ agile methodologies
+      like Scrum and Kanban. Proficient in creating wireframes, site maps, and
+      flowcharts to ensure optimal user experience.
+    </p>
+    <div>
+      <Link
+        to="work"
+        smooth={true}
+        duration={500}
+        delay={300}
+        className="flex items-center"
+      >
+        <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#6BD425] hover:border-[#6BD425] hover:text-[#0e0e0e] font-medium rounded-md duration-700 z-10">
+          View Work
+          <span className="group-hover:rotate-90 duration-300">
+            <HiArrowNarrowRight className="ml-3" />
+          </span>
+        </button>
+      </Link>
     </div>
+  </div>
+</div>
+
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,82 +1,188 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/Logo_.png';
-import { Link } from 'react-scroll';
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/Logo_.png";
+import { Link } from "react-scroll";
 
 function Navbar() {
   const [nav, setNav] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
   const handleClick = () => setNav(!nav);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="fixed w-full h-[65px] flex justify-between items-center md:px-20 bg-[#1a1a1a] text-white z-20">
+    <div
+      className={`fixed w-full h-[65px] flex justify-between items-center md:px-20 ${
+        isScrolled ? "bg-[#1a1a1a]" : "bg-transparent"
+      } text-white z-20 transition-colors duration-300`}
+    >
       <div className="w-[30px] ml-6 md:ml-0">
-        <Link to="home" smooth={true} duration={500}><img src={Logo} alt="Logo Image" className="cursor-pointer hover:scale-110 duration-500 ml-[8px]"/></Link>
+        <Link to="home" smooth={true} duration={500}>
+          <img
+            src={Logo}
+            alt="Logo Image"
+            className="cursor-pointer hover:scale-110 duration-500 ml-[8px]"
+          />
+        </Link>
       </div>
 
       {/* Menu */}
       <ul className="hidden md:flex md:mr-0">
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link to="home" smooth={true} duration={500}>Home</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link to="about" smooth={true} duration={500}>About</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link to="work" smooth={true} duration={500}>Work</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger Button */}
-      <div onClick={handleClick} className="md:hidden mr-6 z-10 cursor-pointer hover:scale-110 duration-500 ml-[8px] text-[26px] font-thin text-[#6BD425] hover:text-[#ffffff]">
+      <div
+        onClick={handleClick}
+        className="md:hidden mr-6 z-10 cursor-pointer hover:scale-110 duration-500 ml-[8px] text-[26px] font-thin text-[#6BD425] hover:text-[#ffffff]"
+      >
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile Menu */}
-      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0e0e0e] opacity-95 flex flex-col justify-center items-center gap-y-6 text-4xl"}>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link onClick={handleClick} to="skills" smooth={true} duration={500}>Skills</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link onClick={handleClick} to="work" smooth={true} duration={500}>Work</Link></li>
-        <li className="hover:text-[#6BD425] hover:scale-110 duration-700"><Link onClick={handleClick} to="contact" smooth={true} duration={500}>Contact</Link></li>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0e0e0e] opacity-95 flex flex-col justify-center items-center gap-y-6 text-4xl"
+        }
+      >
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="hover:text-[#6BD425] hover:scale-110 duration-700">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
       <div className="flex fixed mx-auto bottom-0 flex-row w-full justify-center items-center md:flex md:fixed md:bottom-auto md:flex-col md:top-[35%] md:left-0">
-        
         <ul className="flex w-full md:top-[35%] md:left-0 md:justify-start md:w-full justify-center mx-auto">
-
-          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute
-          md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-0 md:py-1 duration-700 bg-blue-600">
-            <a className="hidden md:flex justify-between items-center w-full text-gray-300"
-            href="https://www.linkedin.com/in/japrweb/" target="_blank">Linkedin <FaLinkedin className="text-[30px]"/></a>
-            {/* Mobile */}
-            <a className="md:hidden text-gray-300"
-            href="https://www.linkedin.com/in/japrweb/" target="_blank"><FaLinkedin className="text-[30px] mx-auto mb-2"/>Linkedin</a>
+          {/* LinkedIn */}
+          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-0 md:py-1 duration-700 bg-blue-600">
+            <a
+              className="hidden md:flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/japrweb/"
+              target="_blank"
+            >
+              Linkedin <FaLinkedin className="text-[30px]" />
+            </a>
+            <a
+              className="md:hidden text-gray-300"
+              href="https://www.linkedin.com/in/japrweb/"
+              target="_blank"
+            >
+              <FaLinkedin className="text-[30px] mx-auto mb-2" />
+              Linkedin
+            </a>
           </li>
-
-          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute
-          md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-[60px] md:py-1 duration-700 bg-[#333333]">
-            <a className="hidden md:flex justify-between items-center w-full text-gray-300"
-            href="https://github.com/japrweb" target="_blank">Github <FaGithub className="text-[30px]"/></a>
-            {/* Mobile */}
-            <a className="md:hidden text-gray-300"
-            href="https://github.com/japrweb" target="_blank"><FaGithub className="text-[30px] mx-auto mb-2"/>Github</a>
+          {/* GitHub */}
+          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-[60px] md:py-1 duration-700 bg-[#333333]">
+            <a
+              className="hidden md:flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/japrweb"
+              target="_blank"
+            >
+              Github <FaGithub className="text-[30px]" />
+            </a>
+            <a
+              className="md:hidden text-gray-300"
+              href="https://github.com/japrweb"
+              target="_blank"
+            >
+              <FaGithub className="text-[30px] mx-auto mb-2" />
+              Github
+            </a>
           </li>
-
-          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute
-          md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-[120px] md:py-1 duration-700 bg-[#945353]">
-            <a className="hidden md:flex justify-between items-center w-full text-gray-300"
-            href="mailto:japr893@gmail.com" target="_blank">Email <HiOutlineMail className="text-[30px]"/></a>
-            {/* Mobile */}
-            <a className="md:hidden text-gray-300"
-            href="mailto:japr893@gmail.com" target="_blank"><HiOutlineMail className="text-[30px] mx-auto mb-2"/>Email</a>
+          {/* Email */}
+          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-[120px] md:py-1 duration-700 bg-[#945353]">
+            <a
+              className="hidden md:flex justify-between items-center w-full text-gray-300"
+              href="mailto:japr893@gmail.com"
+              target="_blank"
+            >
+              Email <HiOutlineMail className="text-[30px]" />
+            </a>
+            <a
+              className="md:hidden text-gray-300"
+              href="mailto:japr893@gmail.com"
+              target="_blank"
+            >
+              <HiOutlineMail className="text-[30px] mx-auto mb-2" />
+              Email
+            </a>
           </li>
-
-          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute
-          md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-[180px] md:py-1 duration-700 bg-[#539486]">
-            <a className="hidden md:flex justify-between items-center w-full text-gray-300 ml-1.5"
-            href="./assets/Resume_JohnAPerezR.pdf" download>Resume<BsFillPersonLinesFill className="text-[30px]"/></a>
-            {/* Mobile */}
-            <a className="md:hidden text-gray-300"
-            href="./assets/Resume_JohnAPerezR.pdf" download><BsFillPersonLinesFill className="text-[30px] mx-auto mb-2"/>Resume</a>
+          {/* Resume */}
+          <li className="flex justify-center w-1/4 self-center h-[90px] items-start pt-4 mx-auto relative bottom-[-35px] hover:bottom-0 md:absolute md:w-[160px] md:h-[60px] md:items-center md:ml-[-100px] md:hover:ml-[-10px] md:top-[180px] md:py-1 duration-700 bg-[#539486]">
+            <a
+              className="hidden md:flex justify-between items-center w-full text-gray-300 ml-1.5"
+              href="./assets/Resume_JohnAPerezR.pdf"
+              download
+            >
+              Resume
+              <BsFillPersonLinesFill className="text-[30px]" />
+            </a>
+            <a
+              className="md:hidden text-gray-300"
+              href="./assets/Resume_JohnAPerezR.pdf"
+              download
+            >
+              <BsFillPersonLinesFill className="text-[30px] mx-auto mb-2" />
+              Resume
+            </a>
           </li>
         </ul>
       </div>
