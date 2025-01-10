33a78c4ce8fbee514ed694f86bbdee1093b3c734
John <japr893@gmail.com>
2025-01-10 08:38:40 -0300

    fontAwesome

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,47 +7,36 @@ function About() {
       className="w-full h-full bg-[#1a1a1a] text-gray-300 md:m-auto"
     >
       <div className="flex flex-col justify-center items-center w-full h-full">
-            <p className="text-4xl text-center font-black inline">SOBRE MÍ</p>
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
-          </div>
-          <div></div>
-        </div>
+              <i class="fa-solid fa-microchip text-4xl mb-[0.5em] text-[#2c2c2c]"></i>
+            <p className="text-4xl text-center font-black inline">
+              SOBRE MÍ
+            </p>
+        <div className="max-w-[1000px] w-full  gap-8">
+          <div className="sm: pb-8 pl-4">
 
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="sm:text-right text-4xl font-bold">
-            <p>
-              <i class="fa-solid fa-microchip"></i>
-              <i class="fa-solid fa-user text-4xl mr-4 text-gray-300"></i> 
-              Hi. I'm John. <br />
-              Please take a look around.
-            </p>
+          <p className="mb-2">
+          Mi nombre es John, soy un desarrollador front-end, mi enfoque es crear experiencias atractivas y funcionales. Me especializo en el diseño y desarrollo de sitios web que combinan estética y usabilidad, asegurando una navegación intuitiva para los usuarios.
+  <h3>Mis habilidades incluyen:</h3>
+  <ul>
+    <li>Comunicación efectiva para comprender y plasmar las necesidades del cliente.</li>
+    <li>Trabajo en equipo orientado a alcanzar objetivos comunes.</li>
+    <li>Resolución de problemas y toma de decisiones críticas en momentos clave.</li>
+    <li>Adaptabilidad a cambios y nuevos desafíos en entornos dinámicos.</li>
+    <li>Análisis y pensamiento crítico para optimizar soluciones.</li>
+    <li>Compromiso con el aprendizaje continuo, manteniéndome actualizado con las últimas tendencias y tecnologías.</li>
+  </ul>
+  <p>Mi experiencia en negociación y gestión de proyectos me permite colaborar estrechamente con equipos multidisciplinarios, garantizando el cumplimiento de metas y la satisfacción de las expectativas del cliente. Siempre estoy en busca de nuevos retos y oportunidades que me permitan crecer en el ámbito tecnológico.</p>
+  <p>Si deseas conocer más sobre mi trabajo o explorar posibles colaboraciones, te invito a revisar mis proyectos y a ponerte en contacto conmigo.</p>
+
+  </p>
+
+
           </div>
-          <div>
-            <p>
-              In terms of my personal skills, I am characterized by:
-              <br /> <br />
-              <ul>
-                <li>✖ My ability to communicate effectively</li>
-                <li>✖ Working in a team to achieve common goals</li>
-                <li>✖ Solving problems and making critical decisions</li>
-                <li>✖ Easily adapting to changes and new challenges</li>
-                <li>
-                  ✖ Analyzing and thinking critically to improve solutions
-                </li>
-                <li>
-                  ✖ Being in a constant learning process to stay up to date with
-                  the latest trends and technologies
-                </li>
-              </ul>{" "}
-              <br />
-              My ability to negotiate and manage projects allows me to work in
-              collaboration with my team to achieve the set goals and meet the
-              client's expectations. I am always looking for new challenges and
-              opportunities to continue growing in the field of technology.
-            </p>
-          </div>
-        </div>
+
+        </div>        
+
+
+
       </div>
     </div>
   );
