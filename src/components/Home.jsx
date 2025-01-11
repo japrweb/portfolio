7f7bcac4630b05aae926c503b1e2cff67a0b6296
John A. Pérez R. <74509783+japrweb@users.noreply.github.com>
2025-01-11 15:48:33 +0000

    homeDescriptionChanged

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,12 +13,14 @@ function Home() {
 
   {/* Container */}
   <div className="relative max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full items-center ">
-    <h1 className="text-2xl sm:text-4xl font-bold text-[#1A7F64] text-center">Que bueno verte por aquí</h1>
-    <h6 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] text-center">Creemos la web que necesitas</h6>
+    <h1 className="text-2xl sm:text-4xl font-bold text-[#27af8b] text-center mb-2">Que bueno verte por aquí</h1>
+    <h6 className="text-4xl sm:text-7xl font-bold text-[#ffffff] text-center">Creemos la web que necesitas</h6>
     <p className="text-gray-300 py-4 max-w-[630px] text-center">
-    Me apasiona transformar ideas en realidades digitales. No solo diseño, creo experiencias que impulsan tu negocio al siguiente nivel.
-    Explora, inspírate, y cuando estés listo, hablemos de cómo puedo dar vida a tu visión.
+      Transformo tus ideas en soluciones digitales efectivas. No solo diseño, creo experiencias que impulsan tu negocio al siguiente nivel.
+      <br />
+      Explora, inspírate y cuando estés listo, conversemos sobre cómo materializar tu visión.
     </p>
+
     <div>
       <Link
         to="work"
